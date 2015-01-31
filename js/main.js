7edf52ee136b2998fcfd9690f9f056402b1a6d83
Garrett Graham <ganderson7197@icloud.com>
2015-01-31 10:22:47 -0500

    so i pushed the code in the wrong js file. pushing the javascript piece rendering into the main folder. our friend Dan helped us understand how we should render the pieces in JS which we were able to share with the others in the cohort

--- js/main.js
+++ js/main.js
@@ -1,17 +1,51 @@
-document.write('<table>');
-for (var i = 1; i < 9; i++) {
-    document.write('<tr>');
-    for (var j = 1; j < 9; j++) {
-        if (i%2 == j%2) {
-            var class="white";
-        } else {
-            var class="black";
-        } 
-        document.write('td class="' + class + '"></td>');
-    }
-    document.write('</tr>');
-}
-document.write('</table>');
+var generatePieces = function() {
+    // have 8 * 8 loop make all 32 pieces
+    
+    // rooks
+    pieces.push(new piece('rook', 'white', 0, 0));
+    pieces.push(new piece('rook', 'white', 7, 0));
+    pieces.push(new piece('rook', 'black', 0, 7));
+    pieces.push(new piece('rook', 'black', 7, 7));
+    
+    // knights
+    pieces.push(new piece('knight', 'white', 1, 0));
+    pieces.push(new piece('knight', 'white', 6, 0));
+    pieces.push(new piece('knight', 'black', 7, 6));
+    pieces.push(new piece('knight', 'black', 7, 1));
+
+    // bishops
+    pieces.push(new piece('bishop', 'white', 0, 0));
+    pieces.push(new piece('bishop', 'white', 7, 0));
+    pieces.push(new piece('bishop', 'black', 0, 7));
+    pieces.push(new piece('bishop', 'black', 7, 7));
+
+    // queens
+    pieces.push(new piece('queen', 'white', 3, 0));
+    pieces.push(new piece('queen', 'black', 3, 7));
+
+    // kings
+    pieces.push(new piece('king', 'white', 4, 0));
+    pieces.push(new piece('king', 'black', 4, 7));
+
+    // pawns
+    pieces.push(new piece('pawn', 'white', 0, 1));
+    pieces.push(new piece('pawn', 'white', 1, 1));
+    pieces.push(new piece('pawn', 'white', 2, 1));
+    pieces.push(new piece('pawn', 'white', 3, 1));
+    pieces.push(new piece('pawn', 'white', 4, 1));
+    pieces.push(new piece('pawn', 'white', 5, 1));
+    pieces.push(new piece('pawn', 'white', 6, 1));
+    pieces.push(new piece('pawn', 'white', 7, 1));
+    pieces.push(new piece('pawn', 'black', 0, 6));
+    pieces.push(new piece('pawn', 'black', 1, 6));
+    pieces.push(new piece('pawn', 'black', 2, 6));
+    pieces.push(new piece('pawn', 'black', 3, 6));
+    pieces.push(new piece('pawn', 'black', 4, 6));
+    pieces.push(new piece('pawn', 'black', 5, 6));
+    pieces.push(new piece('pawn', 'black', 6, 6));
+    pieces.push(new piece('pawn', 'black', 7, 6));
+
+};
 //      var table = document.body.children[0],
 //          thead = document.body.children[0].children[0],
 //          tbody = document.body.children[0].children[1];
