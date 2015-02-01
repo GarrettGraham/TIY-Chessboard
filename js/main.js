7bf9590867f1d17ef44fbadeca1f235302b5e78c
Garrett Graham <ganderson7197@icloud.com>
2015-02-01 14:19:43 -0500

    @jgarcia99 fixed the code in the js file for the pieces with the appropriate opening and closing tags. added the span class to the black and white pawns.  the chess is not rendering with the grid or the pieces etc but not going to worry about that since for the most part the code is correct. dont think its a src code issue because all of the files seem to be linked correctly.

--- js/main.js
+++ js/main.js
@@ -21,23 +21,23 @@ var board = {};
 board.a = {
     'one': '<span class="">&9820;</span>',
     'two': '<span class="">&9822;</span>',
-    'three': 'span class="">&9821;</span>',
-    'four': 'span class="">&98819;</span>',
-    'five': 'span class="">&9818;</span>',
-    'six': 'span class="">&9821;</span>',
-    'seven': 'span class="">&9822;</span>',
-    'eight': 'span class="">&9820;</span>',
+    'three': '<span class="">&9821;</span>',
+    'four': '<span class="">&98819;</span>',
+    'five': '<span class="">&9818;</span>',
+    'six': '<span class="">&9821;</span>',
+    'seven': '<span class="">&9822;</span>',
+    'eight': '<span class="">&9820;</span>',
 };
 
 board.b = {
-    'one': '&#9823;',
-    'two': '&#9823;',
-    'three': '&#9823;',
-    'four': '&#9823;',
-    'five': '&#9823;',
-    'six': '&#9823;',
-    'seven': '&#9823;',
-    'eight': '&#9823;'
+    'one': '<span class="">&#9823;</span>',
+    'two': '<span class="">&#9823;</span>',
+    'three': '<span class="">&#9823;</span>',
+    'four': '<span class="">&#9823;</span>',
+    'five': '<span class="">&#9823;</span>',
+    'six': '<span class="">&#9823;</span>',
+    'seven': '<span class="">&#9823;</span>',
+    'eight': '<span class="">&#9823;</span>'
    
 };
 
@@ -86,26 +86,26 @@ board.f = {
 };
 
 board.g = {
-    'one': '&#9817;',
-    'two': '&#9817;',
-    'three': '&#9817;',
-    'four': '&#9817;',
-    'five': '&#9817;',
-    'six': '&#9817;',
-    'seven': '&#9817;',
-    'eight': '&#9817;'  
+    'one': '<span class="">&#9817;</span>',
+    'two': '<span class="">&#9817;</span>',
+    'three': '<span class="">&#9817;</span>',
+    'four': '<span class="">&#9817;</span>',
+    'five': '<span class="">&#9817;</span>',
+    'six': '<span class="">&#9817;</span>',
+    'seven': '<span class="">&#9817;</span>',
+    'eight': '<span class="">&#9817;</span>'  
     
 };
 
 board.h = {
     'one': '<span class="">&9814;</span>',
     'two': '<span class="">&9816;</span>',
-    'three': 'span class="">&9815;</span>',
-    'four': 'span class="">&98813;</span>',
-    'five': 'span class="">&9812;</span>',
-    'six': 'span class="">&9815;</span>',
-    'seven': 'span class="">&9816;</span>',
-    'eight': 'span class="">&9814;</span>',
+    'three': '<span class="">&9815;</span>',
+    'four': '<span class="">&98813;</span>',
+    'five': '<span class="">&9812;</span>',
+    'six': '<span class="">&9815;</span>',
+    'seven': '<span class="">&9816;</span>',
+    'eight': '<span class="">&9814;</span>',
 
 };
 
