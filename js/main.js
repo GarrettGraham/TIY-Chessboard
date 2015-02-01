4a9ee808b1da54676b1bafa74e14370fa9cbf6c1
Josuel C Garcia <garciajosuel@yahoo.com>
2015-02-01 12:37:35 -0500

    finished the black main chess pieces without pawns

--- js/main.js
+++ js/main.js
@@ -2,6 +2,13 @@ var board = {};
 
 board.a = {
     'one': '<span class="">&9820;</span>',
-    'two': '<span class="">&9820;</span>',
-    'three
-}
+    'two': '<span class="">&9822;</span>',
+    'three': 'span class="">&9821;</span>',
+    'four': 'span class="">&98819;</span>',
+    'five': 'span class="">&9818;</span>',
+    'six': 'span class="">&9821;</span>',
+    'seven': 'span class="">&9822;</span>',
+    'eight': 'span class="">&9820;</span>',
+};
+
+
